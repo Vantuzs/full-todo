0681f1a920baa7733c5ad8735ef6496b97d33148
Vantuz <Shcherbina444@gmail.com>
2025-05-17 13:29:14 +0300

    Redux welcome

--- client/src/Components/Counter.js
+++ client/src/Components/Counter.js
@@ -1,48 +1,59 @@
 import React, { useReducer, useState } from 'react';
+import {connect} from 'react-redux'
 
-const reducer = (state,action) =>{ // action.type - что случилось, action.payload - доп дынные 
-    switch(action.type) {
-        case 'COUNTER_PLUS': {
-            return {
-                counter: state.counter + 1
-            };
-        }
-        case 'COUNTER_MINUS': {
-            return {
-                counter: state.counter - 1
-            }
-        }
-        default: return state;
-    }
-}
 
-const Counter = () => {
-    const [state,dispatch] = useReducer(reducer,{
-        counter: 0
-    })
 
+const Counter = (props) => {
+    
     const increment = ()=>{
         const action = {
             type: 'COUNTER_PLUS'
         }
-        dispatch(action)
+        props.dispatch(action);
     }
-
     const decrement = ()=>{
         const action = {
             type: 'COUNTER_MINUS'
         }
-        dispatch(action)
+        props.dispatch(action);
     }
-
-
+    
+    
+    console.log(props)
     return (
         <>
-            <h1>{state.counter}</h1>
+            <h1>{props.counter}</h1>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
         </>
     );
 }
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return state
+}
+
+const WrappedCounter = connect(mapStateToProps)(Counter);
+
+export default WrappedCounter;
+
+/* 
+
+connect - функция, которя принимает 2 опциональне аргументы и подписывает компоненту на обновление стейта
+
+- mapStateToProps
+Функция, которая принимает ВЕСЬ стейт и возвращает только ту часть стейта, которая нужжна именно этой компоненте
+
+Кариювання фукнций - трансфо
+f(a,b,c) -> f(a)(b)(c)
+
+
+function add(x) {
+    return function(y) {
+        return x+y
+    }
+}
+add(2)(3)
+result = 5
+
+*/
